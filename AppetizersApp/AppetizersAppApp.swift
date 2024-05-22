77575b3d88609ea9857b247f473231fa3c8dc7e6
Elish <medioni@g.jct.ac.il>
2024-05-22 17:15:02 +0300

    Initial Commit

---
+++ AppetizersApp/AppetizersAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersAppApp.swift
+//  AppetizersApp
+//
+//  Created by Elisheva Medioni on 22/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
